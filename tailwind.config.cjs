163e39d7cf37aeade9531cf7a38e968c150a464d
Santiao Ordoñez <sa.an.or.pu@gmail.com>
2025-03-29 23:22:30 -0500

    Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,24 @@
-module.exports = {
-    content: ["./src/**/*.{astro,html,js,ts}"],
-    theme: {
-      extend: {},
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    plugins: [],
-  };
+    extend: {
+      fontFamily: {
+        'Geist': ['"Big Shoulders"', 'sans-serif'],
+      },
+      colors: {
+        'color1': '#e7e7e9',
+        'color2': '#71717A',
+        'color-blue': '#122191'
+      },
+    },
+  },
+  plugins: [],
+}
